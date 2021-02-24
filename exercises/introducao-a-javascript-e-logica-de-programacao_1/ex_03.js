cfe960ea7d5cef848d53e3c9a20484750be84535
Elisa <elisafranca.carvalho@gmail.com>
2021-02-24 20:41:55 -0300

    Cria programa 03: Maior de tres numeros

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex_03.js
@@ -0,0 +1,14 @@
+let numberOne = 45;
+let numberTwo = 30;
+let numberThree = 28;
+
+if (numberOne > numberTwo && numberOne > numberThree) {
+   console.log("O maior número é:" , numberOne);
+}
+
+else if (numberTwo > numberOne && numberTwo > numberThree) {
+   console.log("O maior número é:" , numberTwo);
+}
+else {
+    console.log("O maior número é:" , numberThree);
+}
