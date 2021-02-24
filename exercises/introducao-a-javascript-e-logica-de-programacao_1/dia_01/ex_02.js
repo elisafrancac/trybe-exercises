6d20b9618d4ab1ae5cf5aee4fb28692853e24043
Elisa <elisafranca.carvalho@gmail.com>
2021-02-24 20:35:41 -0300

    Cria programa 02: Maior de dois numeros

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/dia_01/ex_02.js
@@ -0,0 +1,10 @@
+let numberOne = 13;
+let numberTwo = 30;
+
+if (numberOne > numberTwo) {
+   console.log("O maior número é:" , numberOne);
+}
+
+else {
+    console.log("O maior número é:" , numberTwo);
+}
