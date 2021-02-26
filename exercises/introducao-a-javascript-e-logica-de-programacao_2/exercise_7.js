6a7b1ac0a8073c4c32c06d364fc187b50c596ba7
Elisa <elisafranca.carvalho@gmail.com>
2021-02-26 13:35:01 -0300

    Corrige exercise 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_7.js
@@ -1,11 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let atual = 0;
-let smallestNumber = 0;
-
-for (let index = 0; index < numbers.length; index = index + 1){
-    atual = numbers[index];
-    if (atual < numbers[index + 1]) {
-        smallestNumber = atual;
-    }
-}   
+let smallestNumber = numbers[0];
+for (let index = 0; index < numbers.length; index += 1) {
+   if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+   }
+}
 console.log(smallestNumber);
