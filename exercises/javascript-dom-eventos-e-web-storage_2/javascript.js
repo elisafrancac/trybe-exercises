fdbd96cefd630af55cbdfaeac6ffe1a68dfcc06c
Elisa <elisafranca.carvalho@gmail.com>
2021-03-04 18:58:45 -0300

    Adiciona a tag h1 como filho da tag body

---
+++ exercises/javascript-dom-eventos-e-web-storage_2/javascript.js
@@ -0,0 +1,6 @@
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let exerciseTitle = document.createElement('h1');
+document.body.appendChild(exerciseTitle);
+exerciseTitle.innerHTML = "Exercício 5.2 - JavaScript DOM";
+
+
