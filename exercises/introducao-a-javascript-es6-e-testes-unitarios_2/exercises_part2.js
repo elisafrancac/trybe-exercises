b9c95e554162caa97a8e8307dd59a45ecd85b4a6
Elisa <elisafranca.carvalho@gmail.com>
2021-03-30 13:03:45 -0300

    Parte 2: Exercicio 6

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercises_part2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercises_part2.js
@@ -1,34 +1,45 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
 
 const insertShift = (object, newKey, newValue) => {object[newKey] = newValue; return object;}
- console.log(insertShift(lesson2, 'turno', 'manhã'));
+//  console.log(insertShift(lesson2, 'turno', 'manhã'));
 
  const listKeys = (object) => { return Object.keys(object);}
- console.log(listKeys(lesson1));
+//  console.log(listKeys(lesson1));
 
 const objectSize = (object) => { return Object.keys(object).length }
- console.log(objectSize(lesson3));
+//  console.log(objectSize(lesson3));
 
 const listValues = (object) => { return Object.values(object);}
-console.log(listValues(lesson1));
+// console.log(listValues(lesson1));
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
-console.log(allLessons);
+// console.log(allLessons);
+
+const allStudents = () => {
+  const lessons = Object.keys(allLessons);
+  let studentsNumber = 0;
+  lessons.forEach((lessonKey) => {
+    const lessonItem = allLessons[lessonKey];
+    studentsNumber += lessonItem.numeroEstudantes;
+  })
+  return studentsNumber;
+}
+console.log(allStudents());
