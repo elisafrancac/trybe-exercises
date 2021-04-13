9259f4ef40d70bfd71f3fe331e47b57cb1a7a401
Elisa <elisafranca.carvalho@gmail.com>
2021-04-13 10:55:02 -0300

    Exercicio 3

---
+++ exercises/bloco_8/higher-order-functions-do-javascript-es6_1/exercise3.js
@@ -0,0 +1,13 @@
+// Crie uma HOF utilizando o conceito de currying . Passe como parâmetros a resposta correta 
+// (Ex: Gabarito) para uma pergunta e uma resposta a ser validada (Ex: Resposta de uma pessoa). 
+// Sua HOF deve checar se a resposta enviada pela pessoa usuária é correta. Se for, a função retorna 
+// true , se não for, a função retorna false.
+
+// Sugestão de respostas a serem validadas.
+const correctAns = 'higher order function';
+const userAns = 'HIGHER ORDER FUNCTION';
+
+const checkAnswer = (correctAnswer) => (userAnswer) => {
+  return correctAnswer === userAnswer.toLowerCase();  
+} 
+console.log(checkAnswer(correctAns)(userAns));
