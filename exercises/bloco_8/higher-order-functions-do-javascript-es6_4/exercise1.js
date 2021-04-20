5d9083642c2e9a24638ff932e6d501aeedc90285
Elisa <elisafranca.carvalho@gmail.com>
2021-04-19 21:48:05 -0300

    Exercicio 1: Dada uma matriz de matrizes, transforma em uma única matriz

---
+++ exercises/bloco_8/higher-order-functions-do-javascript-es6_4/exercise1.js
@@ -0,0 +1,15 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+function flatten() {
+    return arrays.reduce((acumulator, index) => acumulator.concat(index), []);
+}
+console.log(flatten());
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
