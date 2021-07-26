0b3d68db0a5e6e198780fde5c1df92e35e7fa409
Elisa <elisafranca.carvalho@gmail.com>
2021-07-26 09:55:35 -0300

    feat: exercise 8

--- exercises/back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercise7.sql
+++ exercises/back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercise7.sql
@@ -1,3 +1,5 @@
+-- Escreva uma query que exiba a quantidade de dinheiro necessária 
+-- para efetuar o pagamento de cada profissão ( JOB_ID ).
 SELECT job_id, SUM(salary) AS total_by_job
 FROM hr.employees
 GROUP BY job_id;
