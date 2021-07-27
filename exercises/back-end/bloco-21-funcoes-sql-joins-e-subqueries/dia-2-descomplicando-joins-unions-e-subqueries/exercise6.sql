35ac05ddf630e1e886734a844c09c8fb86b516e1
Elisa <elisafranca.carvalho@gmail.com>
2021-07-27 19:25:10 -0300

    feat: exercise 6 done

--- exercises/back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercise6.sql
+++ exercises/back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercise6.sql
@@ -9,4 +9,12 @@ WHERE id
             Pixar.BoxOffice
         WHERE
             rating > 7.5);
-            
+
+
+SELECT 
+    m.title
+FROM Pixar.Movies AS m
+    INNER JOIN
+		Pixar.BoxOffice AS b ON b.movie_id = m.id
+	WHERE
+		b.rating > 7.5;            
