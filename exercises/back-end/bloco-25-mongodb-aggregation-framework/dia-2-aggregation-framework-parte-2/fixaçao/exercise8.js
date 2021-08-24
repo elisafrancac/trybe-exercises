a94f50b5b30d92f8737950111173ffca96781065
Elisa <elisafranca.carvalho@gmail.com>
2021-08-24 17:20:36 -0300

    feat: exercise8 - complementary exercises

---
+++ exercises/back-end/bloco-25-mongodb-aggregation-framework/dia-2-aggregation-framework-parte-2/fixaçao/exercise8.js
@@ -0,0 +1,12 @@
+// Utilizando o banco de dados storage , faça o seguinte exercício:
+// Calcule o valor total do estoque, considerando que cada produto 
+// valha o mesmo que seu preço de venda. Lembre-se da quantidade.
+db.products.aggregate([
+  {
+    $addFields: {
+      stockTotalValue: {
+        $multiply: ["$sale_price", "$quantity"]
+      },
+    },
+  },
+]);
