9a7fc74d2db6cb9e57fe3c3e391120878d9b6edc
Elisa <elisafranca.carvalho@gmail.com>
2021-08-24 17:17:10 -0300

    feat: exercise7 - complementary exercises

---
+++ exercises/back-end/bloco-25-mongodb-aggregation-framework/dia-2-aggregation-framework-parte-2/fixaçao/exercise7.js
@@ -0,0 +1,12 @@
+// Calcule qual será o preço de venda de cada produto caso haja uma 
+// promoção de 50% de desconto.
+db.products.aggregate([
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      priceWithDiscout: { $divide: ["$sale_price", 2] },
+    }
+  }
+]);
+
