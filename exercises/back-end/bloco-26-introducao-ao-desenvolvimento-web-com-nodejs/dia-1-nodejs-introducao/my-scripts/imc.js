5f01a212b2c7c04f28ad32202ce5bdae5dae9cf3
Elisa <elisafranca.carvalho@gmail.com>
2021-08-25 09:55:07 -0300

    feat: exercise3

--- exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/imc.js
+++ exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/imc.js
@@ -6,8 +6,9 @@
 // Comece criando um novo pacote node com npm init e respondendo às perguntas do npm .
 // Por enquanto, não se preocupe em pedir input da pessoa usuária. Utilize valores fixos para peso e altura .
 // Armazene o script no arquivo imc.js .
-const weight = 70;
-const height = 159;
+const readline = require('readline-sync');
+const weight = readline.questionInt('Qual seu peso? ');
+const height = readline.questionInt('Qual sua altura? ');
 const findImc = () => {
   console.log(`Peso: ${weight}, Altura: ${height}`);
   const result = Math.round((weight / Math.pow(height / 100, 2).toFixed(2)));
@@ -17,3 +18,9 @@ findImc();
 
     // Agora, permita que o script seja executado através do comando npm run imc
     // O novo script criado deve conter o comando que chama o node para executar o arquivo imc.js .
+
+// Chegou a hora de tornar nosso script mais interativo! Vamos adicionar input de quem usa.
+// Você já utilizou o pacote readline-sync para esse fim. Que tal utilizar o mesmo pacote?
+// Substitua os valores fixos de peso e altura por dados informados pela pessoa ao responder
+// as perguntas "Qual seu peso?" e "Qual sua altura?" no terminal.
+
