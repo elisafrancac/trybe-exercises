3412687637eb162902e12f417dfee1bcfbfdeb3d
Elisa <elisafranca.carvalho@gmail.com>
2021-08-26 15:19:06 -0300

    feat: exercise 2

--- exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/calc-project/index.js
+++ exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/calc-project/index.js
@@ -17,6 +17,25 @@ const calc = (a, b, c) => {
   });
 }
 
-calc(50, 10, 2)
+// calc(50, 10, 2)
+//   .then(resolve => console.log(resolve))
+//   .catch(reject => console.log(reject));
+
+  // Escreva um código para consumir a função construída no exercício anterior.
+  // Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. 
+  // Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1) .
+  // Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+  // Utilize then e catch para manipular a Promise retornada pela função:
+  // Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+  // Caso a Promise seja resolvida, escreva na tela o resultado do cálculo.
+  const randomNumber = () => Math.random() * 100 + 1;
+
+  const randomCalc = () => {
+    const numbA = randomNumber();
+    const numbB = randomNumber();
+    const numbC = randomNumber();
+    calc(numbA, numbB, numbC)
   .then(resolve => console.log(resolve))
   .catch(reject => console.log(reject));
+  }
+randomCalc();
