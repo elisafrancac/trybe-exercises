273846c74d19ec15772c7d643cd89a78e7894c85
Elisa <elisafranca.carvalho@gmail.com>
2021-08-30 13:59:17 -0300

    feat: exercise 4

---
+++ exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/node-express/exercise4.js
@@ -0,0 +1,14 @@
+// Crie uma rota PUT /users/:name/:age .
+// Sua rota deve retornar o seguinte JSON: { "message": "Seu nome é <name> e você tem <age> anos de idade" } .
+const express = require('express');
+
+const app = express();
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+app.listen(3000, () => {
+  console.log('Aplicação ouvindo na porta 3000');
+});
