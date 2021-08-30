22b99eb7ca675cb4391a5040f4d9bc56cfc8cf43
Elisa <elisafranca.carvalho@gmail.com>
2021-08-30 13:12:21 -0300

    feat: exercise 2

--- exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/node-express/index.js
+++ exercises/back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/node-express/index.js
@@ -12,3 +12,19 @@ app.get('/ping', (_req, res) => {
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000');
 });
+
+// Crie uma rota POST /hello
+// Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
